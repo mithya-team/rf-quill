4e41ac6e8f1236e821ec041f12cef1477979b84a
sac6inarya <sachinkarya333@gmail.com>
2023-02-22 17:49:45 +0530

    used Editor comoponent of Rich Text Editor  instead of RFReactQuill

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import { Formik } from "formik";
 import "./App.css";
-import RFReactQuill from "./RFReactQuill";
+import RichTextEditor from "./RichTextEditor";
+import { FormConfig } from "react-forms-lite";
 
+const myFieldConfig: FormConfig = {
+  type: "rte-quill",
+  valueKey: "rte",
+};
+const fieldProps = {
+  name: "name",
+  label: "label",
+  helperText: "helper text",
+};
+
+const initialValues = {};
+const onSubmit = () => {
+  return;
+};
 function App() {
   return (
-    <div>
-      <RFReactQuill/>
-    </div>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {(formikProps: any) => {
+        return (
+          <div>
+            <RichTextEditor
+              fieldConfig={myFieldConfig}
+              formikProps={formikProps}
+              fieldProps={fieldProps}
+            />
+          </div>
+        );
+      }}
+    </Formik>
   );
 }
 
---
+++ src/RichTextEditor/index.tsx
@@ -0,0 +1,36 @@
+import { FormikProps } from "formik";
+import { FC } from "react";
+import { FieldProps, HelperText } from "react-forms-lite";
+import { Editor, EditorProps } from "@mithya-team/rich-text-editor";
+import clsx from "clsx";
+import "./index.css";
+
+export interface RichTextEditorFieldProps extends EditorProps {
+  name?: string;
+  label?: string;
+  helperText?: string;
+}
+
+export interface RichTextEditorProps extends FieldProps {
+  fieldProps?: RichTextEditorFieldProps;
+}
+
+const RichTextEditor: FC<RichTextEditorProps> = (props) => {
+  const {
+    formikProps = {} as FormikProps<unknown>,
+    fieldProps = {} as RichTextEditorFieldProps,
+  } = props;
+  const { label, name, className } = fieldProps;
+
+  return (
+    <div className={clsx("field-container", className)}>
+      {label && <span className="field-label">{label}</span>}
+
+      <Editor className={`${name}_${className}`} />
+
+      <HelperText fieldProps={fieldProps} formikProps={formikProps} />
+    </div>
+  );
+};
+
+export default RichTextEditor;
